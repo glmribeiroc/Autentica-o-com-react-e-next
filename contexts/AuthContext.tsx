f362c525b77629c332b79865e33ae584f7b0e1a7
Guilherme <glmribeiroc@gmail.com>
2022-08-01 17:33:56 -0300

    auth

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,120 @@
+import Router from "next/router";
+import { destroyCookie, parseCookies, setCookie } from "nookies";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/apiClient";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+type AuthContextData = {
+  signIn: (credentials: SignInCredentials) => Promise<void>;
+  signOut: () => void;
+  user: User;
+  isAuthenticated: boolean;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+let authChannel: BroadcastChannel;
+
+export function signOut() {
+  destroyCookie(undefined, "nextauth.token");
+  destroyCookie(undefined, "nextauth.refreshToken");
+
+  authChannel.postMessage("signOut");
+
+  Router.push("/");
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+    authChannel = new BroadcastChannel("auth");
+
+    authChannel.onmessage = (message) => {
+      switch (message.data) {
+        case "signOut":
+          Router.reload();
+          break;
+        case "signIn":
+          Router.reload();
+          break;
+        default:
+          break;
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies();
+
+    if (token) {
+      api
+        .get("/me")
+        .then((response) => {
+          const { email, permissions, roles } = response.data;
+
+          setUser({
+            email,
+            permissions,
+            roles,
+          });
+        })
+        .catch((error) => {
+          signOut();
+        });
+    }
+  }, []);
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post("sessions", {
+        email,
+        password,
+      });
+
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: "/",
+      });
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: "/",
+      });
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      Router.push("/dashboard");
+
+      authChannel.postMessage("signOut");
+    } catch (error) {}
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, signOut, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
